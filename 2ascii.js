bb35949ebe6c50a250da4c1b9f9c0124e69bd208
skilbjo <john.skilbeck@gmail.com>
2015-11-28 17:55:05 -0800

    stuff

--- 2ascii.js
+++ 2ascii.js
@@ -3,13 +3,10 @@ var
 	path = require('path'),
 	file = '16234664/1445146046901.jpg',
 	location = path.join('./../4chan/backup/data',file),
-	ascii = require('image-to-ascii')
+	ascii = require('ascii'),
+	pic = new ascii(location)
 	;
 
-ascii({
-	path: location, 
-	colored: true,
-	reverse: true
-	},function(err,image){
-	console.log(err || image);
+pic.convert(function(err,image){
+	console.log(image);
 });
